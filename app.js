5971ed89c1794c4cb2e5e155d0e7ac61feae2690
solalura24 <mira.boyles09@gmail.com>
2017-10-04 00:31:53 +0800

    Week3 Assignment

--- app.js
+++ app.js
@@ -1,75 +1,95 @@
 (function() {
-	'use strict';
+'use strict'
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
 
-angular.module('NarrowItDownApp',[])
-	.controller('NarrowItDownController', NarrowItDownController)
-	.service('MenuSearchService', MenuSearchService)
-	.directive('foundItems', FoundItemsDirective);
-
-NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController (menuSearchService){
-	var nidctrl = this;
-
-	nidctrl.searchTerm = "";
-	nidctrl.found = [];
-	nidctrl.displayFound = false;
-	nidctrl.getItems = function (){
-		 menuSearchService.getMatchedMenuItems(nidctrl.searchTerm)
-		 .then(function(foundItems) {
-                nidctrl.found = foundItems;
-                if (!nidctrl.displayFound) {
-                    nidctrl.displayFound = true;
-                }
-            });
-        };
-        nidctrl.removeItem = function(index) {
-            nidctrl.found.splice(index, 1);
-            if (nidctrl.found.length == 0) {
-                    nidctrl.displayFound = false;
-            }
-        };
-	};
-
-MenuSearchService.$inject = ['$http'];
-    function MenuSearchService($http) {
-        var service = this;
-
-        service.getMatchedMenuItems = function(searchTerm) {
-            return $http.get('https://davids-restaurant.herokuapp.com/menu_items.json')
-            .then(function(response) {
-                var items = response.data.menu_items;
-                var foundItems = [];
-
-                for (var i = 0; i < items.length; i++) {
-                    if (searchTerm !== '' && items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
-                        foundItems.push(items[i]);
-                    }
-                }
-                return foundItems;
-            });
-
-          }
-
-        };
- 
-function FoundItemsDirective(){
+function FoundItemsDirective() {
 	var ddo = {
 		restrict: "E",
 		templateUrl:'foundItems.html',
 		scope:{
 			found: '<',
-			displayfoundItems: '<',
+			empty: '<',
 			onRemove: '&'
-		},
+		}
+	};
+	return ddo;
+}
 
-		controller:FoundItemsDirectiveController,
-		controllerAs:'found',
-		bindToController: true
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController (MenuSearchService){
+	var nidctrl = this;
+
+	nidctrl.searchTerm = '';
+	nidctrl.empty = '';
+
+	nidctrl.searchItem = function () {
+
+		if (nidctrl.searchTerm !== '') {
+			var promise = MenuSearchService.getMatchedMenuItems(nidctrl.searchTerm);
+			promise.then(function(result) {
+				nidctrl.found = result;
+				nidctrl.empty = MenuSearchService.isEmpty();
+			})
+			.catch(function(error) {
+			console.log(error);
+			});
+		}
+		else {
+			nidctrl.empty = MenuSearchService.isEmpty();
+			console.log(nidctrl.empty);
+		};
+	};
+        
+        nidctrl.remove = function(itemIndex) {
+           return MenuSearchService.removeItem(itemIndex);
+         }
+ }	
+
+MenuSearchService.$inject = ['$http'];
+function MenuSearchService($http,searchTerm) {
+        var service = this;
+        var foundItems = [];
+        var emptyMessage = 'Nothing Found';
+
+        service.getMatchedMenuItems = function (searchTerm) {
+
+            searchTerm = searchTerm.trim().toLowerCase();
+            
+            return $http ({
+			method: "GET",
+			url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+		})
+		.then(function(response) {
+
+			for(var i=0; i<response.data.menu_items.length; i++) {
+			
+				if (response.data.menu_items[i].description.toLowerCase().indexOf(searchTerm) !== -1) {
+					foundItems.push(response.data.menu_items[i]);
+				}
+				// else{
+				// 	service.isEmpty();
+				// }
+			}
+			return foundItems;
+
+		})
+		.catch(function(errorResponse) {
+			console.log(errorResponse);
+		});		
 	};
 
-	return ddo;
-};
+	service.removeItem = function (itemIndex) {
+		foundItems.splice(itemIndex, 1);
+		return foundItems;
+	};
 
-function foundItemsDirectiveController() {};
+	service.isEmpty = function () {
+		console.log(emptyMessage);
+		return emptyMessage;
+	};
+}
 
 })();
